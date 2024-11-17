3eb295bffad5ad8f541e5eb2f73220f5137b9731
kastastin <kastastin.main@gmail.com>
2024-11-17 21:08:15 +0200

    @click.stop (stopPropagation), :key, filtering (computed), $refs

--- 1/app.js
+++ 1/app.js
@@ -1,12 +1,18 @@
 const App = {
   data: () => ({
-    person: {
-      firsName: 'Bob',
-      lastName: 'Builder',
-      age: 20,
+    items: [1, 2, 3, 4, 5],
+  }),
+  methods: {
+    addItem() {
+      this.items.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
     },
-    colors: ['yellow', 'blue', 'white'],
-  })
-}
+  },
+  computed: {
+    evenItems() {
+      return this.items.filter(i => i % 2 === 0);
+    },
+  },
+};
 
 Vue.createApp(App).mount("#app");
