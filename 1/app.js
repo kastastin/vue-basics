bcb6c06bd54afd86da75f96d591df5641c8c0b95
kastastin <kastastin.main@gmail.com>
2024-11-17 15:36:11 +0200

    computed, watch, v-model two-way binding, style, class

--- 1/app.js
+++ 1/app.js
@@ -8,15 +8,27 @@ const App = {
     }
   },
   methods: {
-    inputChangeHandler(e) {
-      this.inputValue = e.target.value;
-    },
     addNewNote() {
-      this.notes.push(this.inputValue);
-      this.inputValue = "";
+      if (this.inputValue) {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+      }
     },
     deleteNote(index) {
       this.notes.splice(index, 1);
+    },
+    toUpperCase(str) {
+      return str.toUpperCase();
+    }
+  },
+  computed: {
+    doubleCountComputed() {
+      return this.notes.length * 2;
+    }
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) this.inputValue = "";
     }
   }
 }
