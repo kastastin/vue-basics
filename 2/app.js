8b515ae9401b51e834388344b31f32274c2db3c2
kastastin <kastastin.main@gmail.com>
2024-11-19 04:48:55 +0200

    events in action

--- 2/app.js
+++ 2/app.js
@@ -1,14 +1,42 @@
 const app = Vue.createApp({
-  data: () => ({
-    goalA: "Learn Vue",
-    goalB: "Master Vue and build amazing apps",
-    vueLink: "https://vuejs.org/",
-  }),
+  data() {
+    return {
+      counter: 0,
+      inputValue: "☝️ Type something and press 'Enter' to see an output",
+      firstname: "",
+      lastname: "",
+    };
+  },
   methods: {
-    outputGoal() {
-      return Math.random() < 0.5 ? this.goalA : this.goalB;
+    incrementCounter(amount = 1) {
+      this.counter += amount;
+      console.log('------');
+    },
+    decrementCounter(amount = 1) {
+      this.counter -= amount;
+    },
+    setInputValue(e) {
+      this.inputValue = e.target.value;
+      this.resetInput();
+    },
+    resetInput() {
+      this.$refs.input.value = "";
+      this.$refs.input.focus();
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
+  },
+  watch: {
+    counter(newValue) {
+      this.$refs.input.value = newValue;
+    },
+  },
+  computed: {
+    fullname() {
+      return this.firstname + ' ' + this.lastname;
     },
   },
 });
 
-app.mount("#user-goal");
+app.mount('#events');
