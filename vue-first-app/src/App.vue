2f499915adc4734db31deca72754f73603c44b41
kastastin <kastastin.main@gmail.com>
2024-11-21 00:46:03 +0200

    workflow vue3 in Vite

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import FriendContact from '@/components/FriendContact.vue';
+
+  const friends = [
+    {
+      id: 'manuel',
+      name: 'Manuel Lorenz',
+      phone: '01234 5678 991',
+      email: 'manuel@localhost.com'
+    },
+    {
+      id: 'julie',
+      name: 'Julie Jones',
+      phone: '09876 543 221',
+      email: 'julie@localhost.com'
+    }
+  ];
+</script>
+
+<template>
+  <header>
+    <h1>FriendList</h1>
+  </header>
+  <section>
+    <ul>
+      <FriendContact :friend="friend" v-for="friend in friends" key="friend.id" />
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+  header {
+    width: 90%;
+    max-width: 40rem;
+    margin: 3rem auto;
+    padding: 1rem;
+    border-radius: 10px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    background-color: #58004d;
+    color: white;
+    text-align: center;
+  }
+</style>
---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { ref } from 'vue';
+
+  defineProps(['friend']);
+
+  const detailsAreVisible = ref(false);
+
+  const toggleDetails = () => {
+    detailsAreVisible.value = !detailsAreVisible.value;
+  };
+</script>
+
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide Details' : 'Show Details' }}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<style scoped>
+  li {
+    width: 90%;
+    max-width: 40rem;
+    margin: 1rem auto;
+    padding: 1rem;
+    border-radius: 10px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    text-align: center;
+  }
+
+  h2 {
+    margin: 0 0 1rem 0;
+    border-bottom: 4px solid #ccc;
+    color: #58004d;
+    font-size: 2rem;
+  }
+</style>
