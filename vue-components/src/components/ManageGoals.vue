6e7633ee57cb1f6d2c1195b5ff25352fac4b4125
kastastin <kastastin.main@gmail.com>
2024-11-23 02:52:04 +0200

    teleport, <dialog> (modal)

--- vue-components/src/components/ManageGoals.vue
+++ vue-components/src/components/ManageGoals.vue
@@ -1,6 +1,32 @@
+<script setup>
+  import { ref, useTemplateRef } from 'vue';
+  import ErrorAlert from '@/components/ErrorAlert.vue';
+
+  const input = useTemplateRef('goal');
+  const isInputInvalid = ref(false);
+
+  const setGoal = () => {
+    const enteredValue = input.value.value;
+
+    if (!enteredValue) {
+      isInputInvalid.value = true;
+    }
+  };
+
+  const confirmError = () => {
+    isInputInvalid.value = false;
+  };
+</script>
+
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal">
-  </div>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal">
+  <button @click="setGoal">Set Goal</button>
+  <Teleport to="body">
+    <ErrorAlert v-if="isInputInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters.</p>
+      <button @click="confirmError">Okay</button>
+    </ErrorAlert>
+  </Teleport>
 </template>
